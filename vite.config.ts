fc1acaa1b6fddbe429c69a06f9c86f23b029a63d
viethondz <nguyenvietthong10102007@gmail.com>
2026-06-06 11:18:27 +0700

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
+  base: '/vietthong_porfolio/', // Dòng này giúp chạy đúng trên GitHub Pages
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
